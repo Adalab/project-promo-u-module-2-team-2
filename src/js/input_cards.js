0d7473da04e52bf0582972869eb5b79b3bfe11cb
diana_marmol <dianamarmolus@gmail.com>
2023-09-16 12:47:49 +0200

    Fixed card preview code, (still to be added: image and color palette)

--- src/js/input_cards.js
+++ src/js/input_cards.js
@@ -1,61 +1,87 @@
 'use strict';
+// Variables
+const nameInput = document.querySelector('.js_name_input');
+const jobInput = document.querySelector('.js_job_input');
+const emailInput = document.querySelector('.js_email_input');
+const phoneInput = document.querySelector('.js_phone_input');
+const linkedinInput = document.querySelector('.js_linkedin_input');
+const githubInput = document.querySelector('.js_github_input');
 
-//hacer imagen
-//poner variables nombre completo, puesto, email, teléfono, linkedin, github
-//un evento que recoge todos los input
+const namePreviewTitle = document.querySelector('.js_name_preview');
+const jobPreviewTitle = document.querySelector('.js_job_preview');
+const emailPreviewLink = document.querySelector('.js_email_preview');
+const phonePreviewLink = document.querySelector('.js_phone_preview');
+const linkedinPreviewLink = document.querySelector('.js_linkedin_preview');
+const githubPreviewLink = document.querySelector('.js_github_preview');
 
-//si rellenas nombre (currentTarget) contenido != vacío tiene que aparecer en la tarjeta
+// LOS PARÁMETROS DEL OBJETOS TIENEN LOS MISMOS NOMBRES QUE EN EL ANEXO!!
+const data = {
+  palette: 1, // el que viene por defecto
+  name: '',
+  job: '',
+  phone: '',
+  email: '',
+  linkedin: '',
+  github: '',
+  photo: '',
+};
 
-//innerHTML no sabemos muy bien para qué
+// Funciones
+function updatePreview() {
+  if (data.name === '') {
+    namePreviewTitle.innerHTML = 'Nombre Apellido';
+  } else {
+    namePreviewTitle.innerHTML = data.name;
+  }
 
-// const nombreCompleto = document.querySelector('.js-nombre_completo');
-// const nombreValue = document.querySelector('.nombre_value');
-// const puesto = document.querySelector('.js-puesto');
-// const puestoValue = document.querySelector('.puesto_value');
-// const email = document.querySelector('.js-email');
-// const emailValue = document.querySelector('.email_value');
-// const phone = document.querySelector('.js-phone');
-// const phoneValue = document.querySelector('.phone_value');
-// const linkedin = document.querySelector('.js-linkedin');
-// const linkedinValue = document.querySelector('.linkedin_value');
-// const github = document.querySelector('.js-github');
-// const githubValue = document.querySelector('.github_value');
+  if (data.job === '') {
+    jobPreviewTitle.innerHTML = 'Front-end Developer';
+  } else {
+    jobPreviewTitle.innerHTML = data.job;
+  }
 
-// function updateValueNombre(event) {
-//   nombreValue.textContent = event.target.value;
+  emailPreviewLink.href = 'mailto:' + data.email;
+  phonePreviewLink.href =  data.phone;
+  linkedinPreviewLink.href =  'https://linkedin.com/in/' + data.linkedin;
+  githubPreviewLink.href = 'https://github.com/' + data.github;
 
-//   if (event.target.value === '') {
-//     nombreValue.textContent = 'Nombre Apellido';
-//   }
-// }
+}
 
-// function updateValuePuesto(event) {
-//   puestoValue.textContent = event.target.value;
+// Funciones eventos
+function handleInputName() {
+  data.name = nameInput.value;
+  updatePreview();
+}
 
-//   if (event.target.value === '') {
-//     puestoValue.textContent = 'Front Developer';
-//   }
-// }
+function handleInputJob() {
+  data.job = jobInput.value;
+  updatePreview();
+}
 
-// function updateValueEmail(event) {
-//   emailValue.href = event.target.value;
-// }
+function handleInputEmail() {
+  data.email = emailInput.value;
+  updatePreview();
+}
 
-// function updateValuePhone(event) {
-//   phoneValue.href = event.target.value;
-// }
+function handleInputPhone() {
+  data.phone = phoneInput.value;
+  updatePreview();
+}
 
-// function updateValueLinkedin(event) {
-//   linkedinValue.href = event.target.value;
-// }
+function handleInputLinkedin() {
+  data.linkedin = linkedinInput.value;
+  updatePreview();
+}
+function handleInputGithub() {
+  data.github = githubInput.value;
+  updatePreview();
+}
 
-// function updateValueGithub(event) {
-//   githubValue.href = event.target.value;
-// }
-// //evento
-// nombreCompleto.addEventListener('input', updateValueNombre);
-// puesto.addEventListener('input', updateValuePuesto);
-// email.addEventListener('input', updateValueEmail);
-// //phone.addEventListener('input', updateValuePhone);
-// linkedin.addEventListener('input', updateValueLinkedin);
-// github.addEventListener('input', updateValueGithub);
+//Eventos
+nameInput.addEventListener('input', handleInputName);
+jobInput.addEventListener('input', handleInputJob);
+emailInput.addEventListener('input', handleInputEmail);
+phoneInput.addEventListener('input', handleInputPhone);
+linkedinInput.addEventListener('input', handleInputLinkedin);
+githubInput.addEventListener('input', handleInputGithub);
+
